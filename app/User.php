9d7598f2443135c4460710855b24a9f0c35ae27a
arjundubey <arjun.dubey@sjsu.edu>
2017-05-10 18:17:13 -0700

    this is second commit

--- app/User.php
+++ app/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for arrays.
-     *
+     * add them
      * @var array
      */
     protected $hidden = [
